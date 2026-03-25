93a60d0b410f5cdf499f43c5fbbb3faf928856b1
Mpilo <mpilomastermind@gmail.com>
2026-03-25 20:56:45 +0200

    CODEBLOOD: Added login UI

---
+++ database.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDE3BNbrepcvN5ykhG8BaMM-eUBNXtIUrw",
+    authDomain: "codeblooded-f07f6.firebaseapp.com",
+    projectId: "codeblooded-f07f6",
+    storageBucket: "codeblooded-f07f6.firebasestorage.app",
+    messagingSenderId: "143682941397",
+    appId: "1:143682941397:web:4862d89145decb63cd0d5c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
