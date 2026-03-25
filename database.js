21cffc4ed2821f49911678e90399cdbdeeb28134
Mpilo <mpilomastermind@gmail.com>
2026-03-26 00:05:53 +0200

    CODEBLOOD: Login API

--- database.js
+++ database.js
@@ -1,7 +1,14 @@
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp }
+ from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import{
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    OAuthProvider,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -15,3 +22,38 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  //Google
+  document.getElementById("google").onclick=()=>{
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth,provider)
+    .then(result=>{
+        console.log(result.user);
+    }).catch(error=>console.error(error));
+  }
+  //Facebook
+document.getElementById("facebook").onclick=()=>{
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth,provider)
+  };
+  //Microsoft
+document.getElementById("microsoft").onclick=()=>{
+    const provider = new OAuthProvider('microsoft.com');
+    signInWithPopup(auth,provider)
+  };
+  //Apple
+document.getElementById("apple").onclick=()=>{
+    const provider = new OAuthProvider('apple.com');
+    signInWithPopup(auth,provider)
+  };
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.onclick=()=>{signInWithEmailAndPassword(auth,email,password).then((userCredential)=>{
+    alert("Login successful");
+    window.location.href="dashboard.html";
+  }).catch((error)=>{
+    alert(error.message);
+  });}
